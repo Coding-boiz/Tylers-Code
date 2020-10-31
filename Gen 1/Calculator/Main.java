dde935251eb162a1bb74e5c4eddf8ac0816e6b78
FlamedPhoenix54 <72699774+FlamedPhoenix54@users.noreply.github.com>
2020-10-30 23:04:28 -0400

    Update Main.java

--- Gen 1/Calculator/Main.java
+++ Gen 1/Calculator/Main.java
@@ -1,1 +1,7 @@
-
+//yeah I know that this is a bad calculator, you have to edit the code to use it, but it was the original that i made, so it deserves a place here.
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(5+6);
+	}
+}
