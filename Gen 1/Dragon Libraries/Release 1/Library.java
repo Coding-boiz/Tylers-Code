d1cc9d82c68ae55f98f2c002b44a2b8a739005dd
TylerA512 <69872665+TylerA512@users.noreply.github.com>
2020-11-04 13:37:26 -0500

    Create Library.java

---
+++ Gen 1/Dragon Libraries/Release 1/Library.java
@@ -0,0 +1,20 @@
+public class Library{
+  static void print(String h){
+    System.out.println(h);
+  }
+  static void print(int j){
+    System.out.println(j);
+  }
+  static int add(int a, int b){
+    return a+b;
+  }
+  static int sub(int c,int d){
+    return(c-d);
+  }
+  static int div(int e,int f){
+    return(e/f);
+  }
+  static int mul(int g,int h){
+    return(g*h);
+  }
+}
